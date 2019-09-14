224a2d85fbd1b6bab7df49c0ad51a3803a155525
baozouai <baozouai@gmail.com>
2019-09-14 20:59:17 +0800

    add build

---
+++ build/precache-manifest.7f65a6208c063f18c0910e13b93a3584.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bbcd0a70cce9c5bad09eecbfe1c7018",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d7a1424c922efca40bdf",
+    "url": "./static/css/2.12a9a36f.chunk.css"
+  },
+  {
+    "revision": "5d6d3e2c4c17c2d99d37",
+    "url": "./static/css/main.23880460.chunk.css"
+  },
+  {
+    "revision": "d7a1424c922efca40bdf",
+    "url": "./static/js/2.3c4c196b.chunk.js"
+  },
+  {
+    "revision": "5d6d3e2c4c17c2d99d37",
+    "url": "./static/js/main.9ebf1576.chunk.js"
+  },
+  {
+    "revision": "4b3848b8c1ccde280a04",
+    "url": "./static/js/runtime~main.97be6ba8.js"
+  },
+  {
+    "revision": "9f7255553531762f8a2b1290831bf3b1",
+    "url": "./static/media/cart_empty.9f725555.png"
+  }
+]);
