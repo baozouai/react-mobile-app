33de7b01143cfa3a77fd10730edddfd06f1de97b
baozouai <baozouai@gmail.com>
2019-10-08 19:46:33 +0800

    Updates

---
+++ precache-manifest.4d74eeb5cbde8f11deaf3056cc65bcc7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34b5f19e1cf3b8f1ba6d4357b1fec0e8",
+    "url": "/react-mobile-app/index.html"
+  },
+  {
+    "revision": "115164a4ad1a5614860c",
+    "url": "/react-mobile-app/static/css/2.29fab1a3.chunk.css"
+  },
+  {
+    "revision": "4d86db4b4556eb4573c7",
+    "url": "/react-mobile-app/static/css/main.da4debcf.chunk.css"
+  },
+  {
+    "revision": "115164a4ad1a5614860c",
+    "url": "/react-mobile-app/static/js/2.df0bf40d.chunk.js"
+  },
+  {
+    "revision": "4d86db4b4556eb4573c7",
+    "url": "/react-mobile-app/static/js/main.2ca0279d.chunk.js"
+  },
+  {
+    "revision": "0ca03546409fb956576e",
+    "url": "/react-mobile-app/static/js/runtime~main.68af54ea.js"
+  },
+  {
+    "revision": "9f7255553531762f8a2b1290831bf3b1",
+    "url": "/react-mobile-app/static/media/cart_empty.9f725555.png"
+  }
+]);
