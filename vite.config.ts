67e075269307a88c27268bd4fa18f701454ee4f3
baozouai <baozouai@gmail.com>
2023-12-09 13:12:47 +0800

    fix: build

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig, transformWithEsbuild } from 'vite'
 import react from '@vitejs/plugin-react'
+import createImportPlugin from 'vite-plugin-import'
 
 const isProd = process.env.NODE_ENV === 'production'
-const base = isProd ? '/react-mobile-app': undefined
+const base =  isProd ? '/react-mobile-app': undefined
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     {
       name: 'load+transform-js-files-as-jsx',
       async transform(code, id) {
@@ -22,6 +24,15 @@ export default defineConfig({
         });
       },
   },
+  createImportPlugin({
+    babelImportPluginOptions: [
+      {
+        libraryName: 'antd-mobile',
+        libraryDirectory: 'es',
+        style: 'css', // or 'css'
+      },
+    ],
+  }),
 ],
   resolve: {
     alias: {
@@ -37,6 +48,11 @@ export default defineConfig({
     port: 3002
   },
   base,
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    }
+  },
   optimizeDeps: {
     esbuildOptions: {
       loader: {
